8add5218658e3519d974fd030137771d9989f220
Rodrigo Peres <rodrigoperes@ciandt.com>
2023-07-10 12:43:40 -0300

    New models

--- models.py
+++ models.py
@@ -2,8 +2,8 @@ model_names = {
     "davinci": "text-davinci-003",  # 4,097 tokens
     "gpt-3.5": "gpt-3.5-turbo-0613",  # 4,096 tokens
     "gpt-4": "gpt-4-0613",  # 8,192 tokens
-    "text-bison": "text-bison@001",  # Max input token: 8,192 / Max output tokens: 1,024 / Training data: Up to Feb 2023
-    "chat-bison": "chat-bison@001",  # Max input token: 4,096 / Max output tokens: 1,024 / Training data: Up to Feb 2023
-    "claude-instant": "claude-instant-1-100k",
-    "claude-1": "claude-1-100k",
+    "text-bison": "text-bison@001",  # Max input token: 8,192 / Max output tokens: 1,024
+    "chat-bison": "chat-bison@001",  # Max input token: 4,096 / Max output tokens: 1,024
+    "claude-instant": "claude-instant-1.1-100k",
+    "claude-1": "claude-1.3",
 }

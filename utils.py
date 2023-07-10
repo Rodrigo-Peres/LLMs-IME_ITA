797de9792e2de016623435800389787ac99588d6
Rodrigo Peres <rodrigoperes@ciandt.com>
2023-07-10 12:45:40 -0300

    Adds anthropic models

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import json
 import numpy as np
 import openai
 import pandas as pd
@@ -5,7 +6,11 @@ import random
 import tiktoken
 import time
 
-from vertexai.preview.language_models import TextGenerationModel
+from anthropic import Anthropic
+from vertexai.preview.language_models import (
+    ChatModel,
+    TextGenerationModel,
+)
 
 
 QUESTIONS_FILE_PATH = "data/questions_dataset.xlsx"
@@ -15,6 +20,14 @@ INITIAL_DELAY = 1
 BACKOFF_FACTOR = 2
 
 MAX_TOKENS = 4096
+
+
+with open("./credentials/ANTHROPIC_KEY.json") as f:
+    anthropic_key = json.load(f)
+
+with open("./credentials/GPT_SECRET_KEY.json") as f:
+    open_ai = json.load(f)
+openai.api_key = open_ai["API_KEY"]
 
 
 def tokens_num_allowed(string, encoding_name):
@@ -89,11 +102,12 @@ def get_answer(prompt_technique, model, row, temperature=0):
                 prompt = "\n".join(prompt_list + [prompt])
 
             print(prompt)
-            tokens_allowed = tokens_num_allowed(prompt, model)
-            print(f"Tokens left: {tokens_allowed}")
+            if model in ["text-davinci-003", "gpt-3.5-turbo-0613", "gpt-4-0613"]:
+                tokens_allowed = tokens_num_allowed(prompt, model)
+                print(f"Tokens left: {tokens_allowed}")
 
             if model == "text-davinci-003":
-                answer = openai.Completion.create(
+                response = openai.Completion.create(
                     engine=model,
                     prompt=prompt,
                     temperature=temperature,
@@ -101,10 +115,10 @@ def get_answer(prompt_technique, model, row, temperature=0):
                     frequency_penalty=0,
                     max_tokens=tokens_allowed,
                 )
-                return answer.choices[0].text.strip()
+                return response.choices[0].text.strip()
 
             if model in ["gpt-3.5-turbo-0613", "gpt-4-0613"]:
-                answer = openai.ChatCompletion.create(
+                response = openai.ChatCompletion.create(
                     model=model,
                     messages=[
                         {
@@ -116,20 +130,45 @@ def get_answer(prompt_technique, model, row, temperature=0):
                     temperature=temperature,
                     presence_penalty=0,
                     frequency_penalty=0,
-                    max_tokens=tokens_allowed,
+                    max_tokens=tokens_allowed - 42,
                 )
-                return answer.choices[0].message["content"].strip()
+                return response.choices[0].message["content"].strip()
 
             if model == "text-bison@001":
                 modelo_instanciado = TextGenerationModel.from_pretrained(model)
-                answer = modelo_instanciado.predict(
+                response = modelo_instanciado.predict(
                     prompt=prompt,
                     temperature=temperature,
                     max_output_tokens=1024,
                     top_k=1,
-                    top_p=0.1,
+                    top_p=0.8,
                 )
-                return answer.text
+                return response.text.strip()
+
+            if model == "chat-bison@001":
+                chat_model = ChatModel.from_pretrained(model)
+                chat = chat_model.start_chat(
+                    context="Você é um assistente muito capacitado em resolver questões complexas de Matemática, Química, Física e Português.",
+                )
+                response = chat.send_message(
+                    prompt=prompt,
+                    temperature=temperature,
+                    max_output_tokens=1024,
+                    top_k=1,
+                    top_p=0.8,
+                )
+                return response.text.strip()
+
+            if model in ["claude-instant-1.1-100k", "claude-1.3"]:
+                anthropic = Anthropic(api_key=anthropic_key)
+                response = anthropic.completions.create(
+                    model=model,
+                    max_tokens_to_sample=100000,
+                    stop_sequences=None,
+                    temperature=temperature,
+                    prompt=prompt,
+                )
+                return response.completion.strip()
 
         except Exception as e:
             handle_errors(retries, MAX_RETRIES)
@@ -145,18 +184,18 @@ def extract_right_option(answer):
             Extraia do texto somente a letra que representa a resposta correta.\n
             Texto: Resposta: d) [18001, 19000].\n
             D\n\n
-            Texto: e) 72,8.\n
+            Texto: A resposta correta é a letra e) XeF_3^+, SF_4, ClF_3.\n
             E\n\n
             Texto: C) cumprem a função de destacar o absurd\n
             C\n\n
             Texto: {answer}\n
             """
-            answer = openai.ChatCompletion.create(
+            response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo-0613",
                 messages=[{"role": "user", "content": prompt}],
                 temperature=0,
             )
-            return answer.choices[0].message["content"].strip()
+            return response.choices[0].message["content"].strip()
 
         except Exception as e:
             handle_errors(retries, MAX_RETRIES)
